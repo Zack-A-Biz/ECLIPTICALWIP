5aa32ca6008161e08a75f0f475486f803bb75e2f
Squidknee <72271842+Squidknee@users.noreply.github.com>
2025-01-07 20:46:24 -0500

    Initial commit

---
+++ src/main/java/net/squidknee/eclipticalmod/EclipticalModClient.java
@@ -0,0 +1,11 @@
+package net.squidknee.eclipticalmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EclipticalModClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/squidknee/eclipticalmod/EclipticalModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.squidknee.eclipticalmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EclipticalModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
